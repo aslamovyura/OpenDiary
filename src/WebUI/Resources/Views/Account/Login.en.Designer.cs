86a2df4e55c8aed56badd6e59924bf7eba382b9e
Aslamov Yury <aslamov.yura@gmail.com>
2020-05-29 13:29:48 +0300

    Updated localization

--- src/WebUI/Resources/Views/Account/Login.en.Designer.cs
+++ src/WebUI/Resources/Views/Account/Login.en.Designer.cs
@@ -16,21 +16,21 @@ namespace WebUI.Resources.Views.Account {
     [System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
     [System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class SignIn_en {
+    internal class Login_en {
         
         private static System.Resources.ResourceManager resourceMan;
         
         private static System.Globalization.CultureInfo resourceCulture;
         
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal SignIn_en() {
+        internal Login_en() {
         }
         
         [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Advanced)]
         internal static System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.Equals(null, resourceMan)) {
-                    System.Resources.ResourceManager temp = new System.Resources.ResourceManager("WebUI.Resources.Views.Account.SignIn.en", typeof(SignIn_en).Assembly);
+                    System.Resources.ResourceManager temp = new System.Resources.ResourceManager("WebUI.Resources.Views.Account.Login.en", typeof(Login_en).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
--- src/WebUI/Resources/Views/Account/Login.ru.Designer.cs
+++ src/WebUI/Resources/Views/Account/Login.ru.Designer.cs
@@ -16,21 +16,21 @@ namespace WebUI.Resources.Views.Account {
     [System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
     [System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class SignIn_ru {
+    internal class Login_ru {
         
         private static System.Resources.ResourceManager resourceMan;
         
         private static System.Globalization.CultureInfo resourceCulture;
         
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal SignIn_ru() {
+        internal Login_ru() {
         }
         
         [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Advanced)]
         internal static System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.Equals(null, resourceMan)) {
-                    System.Resources.ResourceManager temp = new System.Resources.ResourceManager("WebUI.Resources.Views.Account.SignIn.ru", typeof(SignIn_ru).Assembly);
+                    System.Resources.ResourceManager temp = new System.Resources.ResourceManager("WebUI.Resources.Views.Account.Login.ru", typeof(Login_ru).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
